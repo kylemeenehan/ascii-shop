d2829ec4991b40973346096c03f4c661dd7ebd50
Kyle Meenehan <kylemeenehan@gmail.com>
2017-10-24 13:24:05 +0200

    added comments to the api

--- src/app/services/api/adverts-api.service.ts
+++ src/app/services/api/adverts-api.service.ts
@@ -11,11 +11,14 @@ export class AdvertsApiService {
   constructor(private http: HttpClient) { }
 
   getAdvert(query: number){
-
+    
+    // A promise is used here to demonstrate the use of vanilla js... you don't always need a library!
     const adPromise = new Promise((resolve, reject) => {
       return this.http.get(`http://localhost:8000/ad?r=${query}`, {responseType: 'arraybuffer'}).subscribe((data) => {
-        let arrayBufferView = new Uint8Array( data );
-        let blob = new Blob( [ arrayBufferView ], { type: "image/jpeg" } );
+
+        // The code below is adapted from this gist: https://gist.github.com/candycode/f18ae1767b2b0aba568e
+        let view = new Uint8Array( data );
+        let blob = new Blob( [ view ], { type: "image/jpeg" } );
         let urlCreator = window.URL || window['webkitURL'];
         let url = urlCreator.createObjectURL( blob );
         resolve(url);
